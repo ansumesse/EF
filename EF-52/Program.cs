8c0ef066f47b012542d8d5180ad359a31f8c7323
m-anass <manas8269@gmail.com>
2024-08-16 01:38:52 +0300

    Remove Records

--- EF-52/Program.cs
+++ EF-52/Program.cs
@@ -9,101 +9,106 @@ namespace EF_52
         {
             var context = new AppDbContext();
 
-            // inner join between 3 tables
-            {
-                var Books = context.Books
-                    .Join(
-                    context.Authors,
-                    b => b.AuthorId,
-                    a => a.AuthorId,
-                    (b, a) => new { b.BookId, b.BookName, a.AuthorName, a.AuthorId }
-                    )
-                    .Join(
-                    context.Nationalities,
-                    b => b.AuthorId,
-                    n => n.AuthorId,
-                    (b, n) => new { b.BookId, b.BookName, b.AuthorName, n.NationalityName }
-                    );
-                foreach (var book in Books)
-                    Console.WriteLine($"{book.BookId}, {book.BookName}, {book.AuthorName}, {book.NationalityName}");
-            }
-            // Left Join Ex
-            {
-                var Books2 = context.Books
-                    .GroupJoin(
-                    context.Authors,
-                    b => b.AuthorId,
-                    a => a.AuthorId,
-                    (b, a) => new
-                    {
-                        Books = b,
-                        Authors = a
-                    }
-                    ).SelectMany(
-                    b => b.Authors.DefaultIfEmpty(),
+            //// inner join between 3 tables
+            //{
+            //    var Books = context.Books
+            //        .Join(
+            //        context.Authors,
+            //        b => b.AuthorId,
+            //        a => a.AuthorId,
+            //        (b, a) => new { b.BookId, b.BookName, a.AuthorName, a.AuthorId }
+            //        )
+            //        .Join(
+            //        context.Nationalities,
+            //        b => b.AuthorId,
+            //        n => n.AuthorId,
+            //        (b, n) => new { b.BookId, b.BookName, b.AuthorName, n.NationalityName }
+            //        );
+            //    foreach (var book in Books)
+            //        Console.WriteLine($"{book.BookId}, {book.BookName}, {book.AuthorName}, {book.NationalityName}");
+            //}
+            //// Left Join Ex
+            //{
+            //    var Books2 = context.Books
+            //        .GroupJoin(
+            //        context.Authors,
+            //        b => b.AuthorId,
+            //        a => a.AuthorId,
+            //        (b, a) => new
+            //        {
+            //            Books = b,
+            //            Authors = a
+            //        }
+            //        ).SelectMany(
+            //        b => b.Authors.DefaultIfEmpty(),
 
-                    (b, a) => new { b.Books, Authors = a }
-                    );
-                foreach (var book in Books2)
-                    Console.WriteLine($"{book.Books.BookId}, {book.Books.BookName}, {book.Authors?.AuthorId}");
-            }
-            // Eager Loading it lows the performance because it loads all the Enties even if i didnot use it
-            {
-                var book3 = context.Books.Include(a => a.Author).Single(b => b.AuthorId == 1);
-                Console.WriteLine(book3.Author.AuthorName);
-                var book4 = context.Books
-                    .Include(x => x.Author)
-                    .ThenInclude(x => x.Nationalities)
-                    .Single(x => x.AuthorId == 1);
-                foreach (var item in book4.Author.Nationalities)
-                {
-                    Console.WriteLine($"{book4.Author.AuthorName}, {item.NationalityName}");
-                }
+            //        (b, a) => new { b.Books, Authors = a }
+            //        );
+            //    foreach (var book in Books2)
+            //        Console.WriteLine($"{book.Books.BookId}, {book.Books.BookName}, {book.Authors?.AuthorId}");
+            //}
+            //// Eager Loading it lows the performance because it loads all the Enties even if i didnot use it
+            //{
+            //    var book3 = context.Books.Include(a => a.Author).Single(b => b.AuthorId == 1);
+            //    Console.WriteLine(book3.Author.AuthorName);
+            //    var book4 = context.Books
+            //        .Include(x => x.Author)
+            //        .ThenInclude(x => x.Nationalities)
+            //        .Single(x => x.AuthorId == 1);
+            //    foreach (var item in book4.Author.Nationalities)
+            //    {
+            //        Console.WriteLine($"{book4.Author.AuthorName}, {item.NationalityName}");
+            //    }
 
-            }
-            // Explicit Loading
+            //}
+            //// Explicit Loading
+            //{
+            //    var book = context.Books.First(x => x.AuthorId == 2);
+            //    context.Entry(book).Reference(x => x.Author).Load();
+            //    Console.WriteLine(book.Author.AuthorName);
+            //}
+            //// Ignoring global filter
+            //{
+            //    context.Books.IgnoreQueryFilters().Select(x => x.BookName);
+            //}
+            //// Add New Records and Related Data
+            //{
+            //    context.Authors.Add(
+            //        new Author()
+            //        {
+            //            AuthorName = "Moaz Anas",
+            //            Books = new List<Book>()
+            //            {
+            //                new Book()
+            //                {
+            //                    BookName = "franceais"
+            //                }
+            //            },
+            //            Nationalities = new List<Nationality>()
+            //            {
+            //                new Nationality()
+            //                {
+            //                    NationalityName = "japanese"
+            //                }
+            //            }
+            //        }
+            //        );
+            //    context.SaveChanges();
+            //}
+            //// Update Records
+            //{
+            //    var book = new Book() { BookId = 3, AuthorId = 5};
+            //    context.Update(book); // update the specified columns only and set the rest to null
+            //    var book2 = new Book() { BookId = 5, AuthorId = 5 };
+            //    context.Update(book2);
+            //    context.Entry(book2).Property(x => x.AuthorId).IsModified = true; // update the specified columns and the rest keep it as it is
+            //    context.SaveChanges();
+            //    Console.WriteLine("done");
+            //}
+            // Remove Records
             {
-                var book = context.Books.First(x => x.AuthorId == 2);
-                context.Entry(book).Reference(x => x.Author).Load();
-                Console.WriteLine(book.Author.AuthorName);
-            }
-            // Ignoring global filter
-            {
-                context.Books.IgnoreQueryFilters().Select(x => x.BookName);
-            }
-            // Add New Records and Related Data
-            {
-                context.Authors.Add(
-                    new Author()
-                    {
-                        AuthorName = "Moaz Anas",
-                        Books = new List<Book>()
-                        {
-                            new Book()
-                            {
-                                BookName = "franceais"
-                            }
-                        },
-                        Nationalities = new List<Nationality>()
-                        {
-                            new Nationality()
-                            {
-                                NationalityName = "japanese"
-                            }
-                        }
-                    }
-                    );
+                context.Remove(new Book() { BookId = 11 });
                 context.SaveChanges();
-            }
-            // Update Records
-            {
-                var book = new Book() { BookId = 3, AuthorId = 5};
-                context.Update(book); // update the specified columns only and set the rest to null
-                var book2 = new Book() { BookId = 5, AuthorId = 5 };
-                context.Update(book2);
-                context.Entry(book2).Property(x => x.AuthorId).IsModified = true; // update the specified columns and the rest keep it as it is
-                context.SaveChanges();
-                Console.WriteLine("done");
             }
             Console.ReadKey();
 
--- EF-52/obj/Debug/net8.0/EF-52.AssemblyInfo.cs
+++ EF-52/obj/Debug/net8.0/EF-52.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF-52")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b52c61579de19ba380d943401c2411d9822b36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aebaa8eb089a4fe76acabcb8e43c3b6bb3eacd4")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF-52")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF-52")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
